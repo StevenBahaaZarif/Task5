84f5739db009889e5b474142f7ac37509aee8b02
StevenBahaaZarif <stevenzaicko29@gmail.com>
2023-09-16 02:00:19 +0300

    Adding Extra Files

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent {
+pic1="http://compass.xbox.com/assets/26/ef/26ef022a-0b6b-4994-9296-a5e897db6fe6.png?n=GwG_Blade-X1-Cross-Sell_648x365.png";
+pic2="https://gmedia.playstation.com/is/image/SIEPDC/console-right_@1x?fmt=png-alpha&scl=1";
+pic3="https://www.pngitem.com/pimgs/m/357-3579885_alienware-area-51m-gaming-laptop-hd-png-download.png";
+}
